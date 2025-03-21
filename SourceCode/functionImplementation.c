15f9dda5565c5fac8753ccd956b1b18ce3b1b416
Bilal Umar <bumar@myseneca.ca>
2025-03-21 14:39:16 -0400

    Add files via upload

--- SourceCode/functionImplementation.c
+++ SourceCode/functionImplementation.c
@@ -1,12 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdbool.h>
+#include <float.h>
 #include "dataStructs.h"
 
 
 
 bool checkSpace(struct Truck truck, struct Shipment shipment) {
-	if (truck.weight + shipment.weight <= 5000 && truck.volume + shipment.size <= 250.0) {
+	if ((truck.weight + shipment.weight) <= 2500 && (truck.volume + shipment.size) <= 100.0) {
 		return true;
 	}
 	else {
@@ -76,12 +77,12 @@ bool valid(struct Shipment shipment, struct Map map) {
 	case '23': column = 23; break;
 	case '24': column = 24; break;
 	default: column = -1;
-	}
+	}	
 
-	if (shipment.weight <= 0 || shipment.weight > 5000) {//Set validity to false if incorrect weight
+	if (shipment.weight <= 0 || shipment.weight > 2500) {//Set validity to false if incorrect weight
 		validity = false;
 	}
-	else if (shipment.size != 0.5 && shipment.size != 2 && shipment.size != 5) {//Set validity to false if incorrect size
+	else if (shipment.size != 1 && shipment.size != 2 && shipment.size != 4 && shipment.size != 5) {//Set validity to false if incorrect size
 		validity = false;
 	}
 	else if (row == -1 || column == -1) {//Set validity to false if address is outside of map bounds
@@ -96,3 +97,92 @@ bool valid(struct Shipment shipment, struct Map map) {
 	return validity;
 }
 
+int assignPackage(const struct Map map, struct Truck trucks[], struct Shipment shipment) {
+
+	//for the best truck to be assigned to, per the trucks array
+	int index = -2;
+	double minDistance = DBL_MAX;
+	// If at least one truck has room and the shipment is valid, assign it
+	if (!valid(shipment, map))
+		return -1;
+		
+		//loop through all trucks to find the one with the closest distance to the package
+		for (int i = 0; i < 2; i++) {
+
+			if (!checkSpace(trucks[i], shipment)) continue;
+
+			int closestIndex = getClosestPoint((const struct Route*)&trucks[i].truckRoute, shipment.address);
+
+			//get the distance between the closest point on the given truck route to the address
+			double dist = distance((const struct Point*)&trucks[i].truckRoute.points[closestIndex], 
+				(const struct Point*)&shipment.address);
+
+			//update per each iteration if the distance is smaller than our current minimum
+			if (dist < minDistance) {
+				minDistance = dist;
+				index = i;
+			}
+		}
+
+		if (index != -2) {
+			// Add the shipment to the truck's packages
+			int sz = findNextAvailableSlot(trucks[index].packages, 500);
+			trucks[index].packages[sz] = shipment;
+
+			// Update the truck's weight and volume
+			trucks[index].weight += shipment.weight;
+			trucks[index].volume += shipment.size;
+
+			//blue, green, yellow 
+			printf("Ship on %s LINE", (index == 0) ? "BLUE" : (index == 1) ? "GREEN" : "YELLOW");
+		}
+		//no truck was found as best or avaible
+		else if (index == -2) {
+			printf("Ships tomorrow \n");
+			return -1;
+		}
+
+		//if the address is not directly across/on the line, divert
+		if (minDistance > 1) {
+			divert(map, trucks, shipment, index);
+		
+		} else {
+			printf(", no diversion\n");
+		}
+
+		return index;
+}
+
+
+
+
+int divert(struct Map map, struct Truck trucks[], struct Shipment shipment, int index) {
+
+	int closestIndex = getClosestPoint((const struct Route*)&trucks[index].truckRoute, shipment.address);
+
+	// get the closet point on the given truck route
+	struct Point closestPoint = trucks[index].truckRoute.points[closestIndex];
+
+	//stores a route to the shortest path in a route
+	struct Route diversionPath = shortestPath(&map, closestPoint, shipment.address);
+	if (diversionPath.numPoints == 0) return -1; 
+
+	printf(", divert ");
+	for (int i = 0; i < diversionPath.numPoints; i++) {
+		printf("%d%c, ", diversionPath.points[i].row, diversionPath.points[i].col);
+	}
+	printf("\n");
+
+	return index;
+}
+
+
+ 
+int findNextAvailableSlot(const struct Shipment packages[], int maxPackages) {
+	for (int i = 0; i < maxPackages; i++) {
+		if (packages[i].weight == 0 && packages[i].size == 0) {
+			return i;
+		}
+	}
+	return -1;
+}
